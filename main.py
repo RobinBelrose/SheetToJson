e93fc138af69fc6d98320295bb8e8fb38b3cc208
Robin Belrose <RobinBelrose@gmail.com>
2022-04-20 19:47:34 -0400

    Add Proof of Concept.

---
+++ main.py
@@ -0,0 +1,73 @@
+from __future__ import print_function
+
+import json
+
+import google.oauth2.service_account
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+from flask import Flask
+from json import dumps
+
+
+#Flask Config
+app = Flask(__name__)
+@app.route('/')
+def get_json():
+    return main()
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1rfZE1u9DlUH7WFU2ExDjfx_A0lwrQ6Gt9JeK0IwqJlU'
+SAMPLE_RANGE_NAME = 'Sheet1!A1:F50'
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    '''creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    '''
+    secret_file = "client_secret.json"
+    creds = google.oauth2.service_account.Credentials.from_service_account_file(secret_file, scopes=SCOPES)
+
+    try:
+        service = build('sheets', 'v4', credentials=creds)
+
+        # Call the Sheets API
+        sheet = service.spreadsheets()
+        result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+                                    range=SAMPLE_RANGE_NAME).execute()
+        values = result.get('values', [])
+
+        if not values:
+            print('No data found.')
+            return
+
+        for row in values:
+            print(row)
+        return dumps(values)
+    except HttpError as err:
+        print(err)
+
+
+if __name__ == '__main__':
+    main()
